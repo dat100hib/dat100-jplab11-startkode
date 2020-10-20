4941cb5aac30437f35d7a54d33fa77bd7f648561
Lars Michael Kristensen <lmkr@hvl.no>
2020-10-20 15:22:11 +0200

    Web server implementasjon revidert

--- src/no/hvl/dat100/jplab11/oppgave7/BloggApp.java
+++ src/no/hvl/dat100/jplab11/oppgave7/BloggApp.java
@@ -17,11 +17,11 @@ public class BloggApp {
 
 	public static String toHTML() {
 
-		Tekst innlegg1 = new Tekst(1, "Sven-Olai", "23-10-2019",
+		Tekst innlegg1 = new Tekst(1, "Sven-Olai", "23-10-2020",
 				"Harald, hva er status for den siste obligatoriske innleveringen?");
-		Bilde innlegg2 = new Bilde(2, "Harald", "24-10-2019",
+		Bilde innlegg2 = new Bilde(2, "Harald", "24-10-2020",
 				"Ser bra ut! - har lagt ved output-eksempel fra enhetstester",
-				"https://cdn1.imggmi.com/uploads/2019/10/25/79e039f3f39c957a19a4958c048026bf-full.png");
+				"https://cdn1.bbcode0.com/uploads/2020/10/20/bbaeb2bf000360b087cd5f62a9967d12-full.png");
 
 		innlegg1.doLike();
 		innlegg1.doLike();
