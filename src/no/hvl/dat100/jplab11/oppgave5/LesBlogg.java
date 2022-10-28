b1e1b4d47c1413a2715a1afda9a8dbf7cf691e7f
Maxwell Wogsland <maxwell@wogsland.org>
2022-10-28 16:30:25 +0200

    working on lesblogg slowly getting it

--- src/no/hvl/dat100/jplab11/oppgave5/LesBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave5/LesBlogg.java
@@ -21,14 +21,41 @@ public class LesBlogg {
 	private static String BILDE = "BILDE";
 
 	public static Blogg les(String mappe, String filnavn) {
-		Blogg blogg; // when inizlize give it lenght based of of the file
 		String path = mappe + filnavn;
+		File fil = new File (path);
 		
-		// open and read file
+		try {
+			Scanner leser = new Scanner(fil);
+			int antall = leser.nextInt();
+			Blogg blogg = new Blogg(antall);
+			
+			// Create objects and add them
+			String innleggtype = leser.nextLine();
+			
+			
+			leser.close();
+		} catch (FileNotFoundException e) {
+			System.out.print("nawdawg");
+			return null;
+		}
+		// Read the first line and create list
+		
+
+		// else  {
+			// return null;
+			// String url = "";
+			// Bilde bidle = new Bilde(id, bruker, dato, likes, teksttxt, url)
+		// }
+		
+		// while (leser.hasNextLine()) {
+		// 	String linje = leser.nextLine();
+		//	System.out.print(linje);
+		// }
+		
+		
 		// create objects
 		// leggtil blogg
-		// close file
 		
-		return blogg;
+		return null;
 	}
 }
